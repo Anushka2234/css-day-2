54430da9580e5d1ed997e3e7a59d764268d04401
Anushka2234 <anushakasrivastava18@gmail.com>
2023-05-19 21:22:32 +0530

    learning css

---
+++ fixed positioning.cpp
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSS 201</title>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+            height: 3000px;
+        }
+        .box{
+            background-color: #f5f5f5;
+            border-bottom: 2px solid #ccc;
+            padding: 20px;
+        }
+    </style>
+    
+    
+
+</head>
+<body>
+    <div class="box"></div>
+    
+</body>
+</html>
